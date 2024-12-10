63e1a65c9dc11093d97e2259aa2aec0abcbbfc58
CCS-WIN-111\Intern <Intern>
2024-12-10 16:35:25 +0900

    追加の修正

--- intern/Assets/scripts/SceneLoad.cs
+++ intern/Assets/scripts/SceneLoad.cs
@@ -8,25 +8,13 @@ public class SceneLoad : MonoBehaviour
     // シーンの名前を入力する
     [SerializeField] string sceneName;
 
-    private void Update()
+    private async void Update()
     {
         // 仮でスペースを押したときにシーンを切り替える
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            // コルーチンを使ってシーンをロード
-            StartCoroutine(LoadAsyncScene());
-        }
-    }
-
-    private IEnumerator LoadAsyncScene()
-    {
-        // バックグラウンドでシーンをロード
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
-
-        // 非同期シーンが完全にロードされるまで待つ
-        while (!asyncLoad.isDone)
-        {
-            yield return null;
+            // 完了するまで実行を中断しながらロードする
+            await SceneManager.LoadSceneAsync(sceneName);
         }
     }
 }
