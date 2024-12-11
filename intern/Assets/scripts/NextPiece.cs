6d33c2ed823ad4db94bdf2bad663cc886df9c5f4
CCS-WIN-111\Intern <Intern>
2024-12-11 12:21:42 +0900

    Nextのミノを表示

---
+++ intern/Assets/scripts/NextPiece.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class NextPiece : MonoBehaviour
+{
+    // ミノのプレファブリスト
+    public GameObject[] tetominoes;
+
+    // 後から追加されて前から取り出したいためQueueを使用
+    Queue<GameObject> nextQueue = new Queue<GameObject>();
+
+    // 次のミノを表示する位置の配列
+    public Transform[] previewPositions;
+
+    private void Start()
+    {
+        // 初めは表示する個数分だけ生成
+        for (int i = 0; i < previewPositions.Length; i++)
+        {
+            AddNextPieceToQueue();
+        }
+    }
+
+    // キューの先頭からミノを取得
+    public GameObject GetNextPiece()
+    {
+        GameObject next = nextQueue.Dequeue();
+        AddNextPieceToQueue();
+        UpdatePreviews();
+        return next;
+    }
+
+    // キューにミノを追加
+    private void AddNextPieceToQueue()
+    {
+        int randomIndex = Random.Range(0, tetominoes.Length);
+        nextQueue.Enqueue(tetominoes[randomIndex]);
+    }
+
+    // NextPieceのプレビュー更新
+    private void UpdatePreviews()
+    {
+        int index = 0;
+
+        foreach (GameObject tetomino in nextQueue)
+        {
+            if (index >= previewPositions.Length) break;
+            GameObject preview = Instantiate(tetomino, previewPositions[index].position, Quaternion.identity);
+            foreach (Transform child in preview.transform)
+            {
+                // UI専用レイヤーに変更
+                child.gameObject.layer = LayerMask.NameToLayer("UI");
+            }
+
+            // 操作に反映されないようにスクリプトを削除
+            Destroy(preview.GetComponent<Tetomino>());
+            index++;
+        }
+    }
+}
